2b9fd77460f51319d5ecf1c33482c3eee2b520c0
Tomoya Koike <makeffort134@gmail.com>
2018-08-23 10:56:21 +0900

    arguments for mail title and sender available

--- send_email.py
+++ send_email.py
@@ -3,14 +3,24 @@ from email.mime.text import MIMEText
 from email.header import Header
 
 import const
+import argparse
+
+parser = argparse.ArgumentParser(description='mailer for notifying finish of command')
+parser.add_argument('--title', '-t', default='command finished',
+                    help='mail title')
+parser.add_argument('--send_to', '-s', default='',
+                    help='which address to send')
+args = parser.parse_args()
+title = args.title
+send_to = args.send_to
 
 charset = "iso-2022-jp"
 myMailaddress = const.from_email  # 自分のGmailアドレス
 password = const.password  # Gmailアカウントのパスワード
-toMailaddress = const.to_email  # 送信相手のメールアドレス
+toMailaddress = send_to if send_to else const.to_email  # 送信相手のメールアドレス
 
-msg = MIMEText("ちゃんと送信できていますか？", "plain", charset)  # メール本文
-msg["subject"] = Header("メール送信テスト！".encode(charset), charset)  # メールタイトル
+msg = MIMEText("表題のファイルの実行が完了しました。", "plain", charset)  # メール本文
+msg["subject"] = Header(title.encode(charset), charset)  # メールタイトル
 
 # smtp_obj = smtplib.SMTP("smtp.gmail.com", 587)    # Gmailはポート番号587のTLSをサポートしていない！よくわからなければスルーしてね。
 smtp_obj = smtplib.SMTP_SSL("smtp.gmail.com", 465)  # Gmailでのメール送信は「smtplib.SMTP_SSL」を用いてポート番号465を使用すること。
