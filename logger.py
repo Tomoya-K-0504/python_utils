9b1af004eec3778466d63e3c620f36317ade53dc
Tomoya Koike <makeffort134@gmail.com>
2018-08-24 13:50:30 +0900

    logger file added

---
+++ logger.py
@@ -0,0 +1,31 @@
+import argparse
+import sys
+from logging import getLogger, INFO, DEBUG, ERROR, FileHandler, StreamHandler, Formatter
+
+
+def logger(log_folder):
+    logger = getLogger()
+    logger.setLevel(INFO)
+    formatter = Formatter(fmt='%(asctime)-15s: %(pathname)s:l-%(lineno)d:\n\t[%(levelname)s] %(message)s')
+
+    error_handler = FileHandler(filename=f"{log_folder}/ERROR.log")
+    error_handler.setLevel(ERROR)
+    error_handler.setFormatter(fmt=formatter)
+    logger.addHandler(error_handler)
+
+    debug_handler = FileHandler(filename=f"{log_folder}/DEBUG.log")
+    debug_handler.setLevel(DEBUG)
+    debug_handler.setFormatter(fmt=formatter)
+    logger.addHandler(debug_handler)
+
+    info_handler = FileHandler(filename=f"{log_folder}/INFO.log")
+    info_handler.setLevel(INFO)
+    info_handler.setFormatter(fmt=formatter)
+    logger.addHandler(info_handler)
+
+    stream_info_handler = StreamHandler(stream=sys.stdout)
+    stream_info_handler.setLevel(INFO)
+    stream_info_handler.setFormatter(fmt=formatter)
+    logger.addHandler(stream_info_handler)
+
+    return logger
